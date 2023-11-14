661472c0498cb334092ae9bfbae6f2fcf135f370
Roman <rbaidachenko@gmail.com>
2023-11-14 12:30:32 +0000

    starting challange

---
+++ src/OtherTasks/ClementGraphProblem.java
@@ -0,0 +1,15 @@
+package OtherTasks;
+
+public class ClementGraphProblem {
+    int[][] matrix =
+            {{1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 1, 1},
+            {0, 0, 1, 0, 1, 0},
+            {1, 1, 0, 0, 1, 0},
+            {1, 0, 1, 1, 0, 0},
+            {1, 0, 0, 0, 0, 1}};
+
+    public void removeIslands(int[][] matrix) {
+
+    }
+}
