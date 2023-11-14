9efdb7fd13fcb1933fbf1d5ff1a642f233e3662d
Roman <rbaidachenko@gmail.com>
2023-11-14 16:43:48 +0000

    re-working graphProblem

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import OtherTasks.ClementGraphProblem;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        var islands = new ClementGraphProblem();
+        int[][] matrix =
+                       {{1, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 1, 1, 1},
+                        {0, 0, 1, 0, 1, 0},
+                        {1, 1, 0, 0, 1, 0},
+                        {1, 0, 1, 1, 0, 0},
+                        {1, 0, 0, 0, 0, 1}};
+        System.out.println(Arrays.deepToString(islands.removeIsland(matrix)));
+
+
 
     }
 }
--- src/OtherTasks/ClementGraphProblem.java
+++ src/OtherTasks/ClementGraphProblem.java
@@ -1,15 +1,69 @@
 package OtherTasks;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClementGraphProblem {
     int[][] matrix =
-            {{1, 0, 0, 0, 0, 0},
+           {{1, 0, 0, 0, 0, 0},
             {0, 1, 0, 1, 1, 1},
             {0, 0, 1, 0, 1, 0},
             {1, 1, 0, 0, 1, 0},
             {1, 0, 1, 1, 0, 0},
             {1, 0, 0, 0, 0, 1}};
 
-    public void removeIslands(int[][] matrix) {
+    // if node is 0 - return true;
+    //if node is 1 - check top, bottom, right, left
+    //if isBorder - return false;
+    //
 
+
+    public int[][] removeIsland(int[][] matrix) {
+        var visited = new HashMap<String, Boolean>();
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix[0].length - 1; j++) {
+                if (isIsland(i,j,matrix,visited))
+                    matrix[i][j] = 0;
+            }
+        }
+        return matrix;
+    }
+
+
+    private boolean isIsland(int rowIndex, int columnIndex, int[][] array, HashMap<String, Boolean> visited) {
+        if (rowIndex < 0 || rowIndex == array.length || columnIndex < 0 || columnIndex == array[0].length)
+            return true;
+
+        if (matrix[rowIndex][columnIndex] == 0)
+            return true;
+
+        if (isBorder(rowIndex, columnIndex, array)) {
+            return false;
+        }
+
+        String node = "" + rowIndex + columnIndex;
+        if (visited.containsKey(node))
+            return visited.get(node);
+
+        if (isIsland(rowIndex - 1, columnIndex, array, visited) &&
+            isIsland(rowIndex + 1, columnIndex, array, visited) &&
+            isIsland(rowIndex, columnIndex - 1, array, visited) &&
+            isIsland(rowIndex, columnIndex + 1, array, visited)) {
+            visited.put(node, true);
+            return true;
+        }
+        else {
+            visited.put(node, false);
+            return false;
+        }
+    }
+
+    private boolean isBorder(int rowIndex, int columnIndex, int[][] array) {
+        return  rowIndex == 0 ||
+                rowIndex == array.length - 1 ||
+                columnIndex == 0 ||
+                columnIndex == array[0].length - 1;
     }
 }
