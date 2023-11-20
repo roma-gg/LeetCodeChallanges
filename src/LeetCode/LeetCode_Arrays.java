83962bc092939ebcfd2dc1ccf431464fbcceb33d
Roman <rbaidachenko@gmail.com>
2023-11-20 22:43:20 +0000

    3/75 challanges

---
+++ src/LeetCode/LeetCode_Arrays.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_Arrays {
+
+    // 1071. Greatest Common Divisor of Strings
+    public String gcdOfString (String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+
+        int min = Math.min(str1.length(), str2.length());
+        for (int i = min; i > 0; i--) {
+            if (str1.length() % i == 0 && str2.length() % i == 0)
+                return str1.substring(0, i);
+        }
+        return "";
+
+    }
+
+    //1431. Kids With the Greatest Number of Candies
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max)
+                max = candies[i];
+        }
+        ArrayList<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max)
+                result.add(true);
+            else
+                result.add(false);
+        }
+        return result;
+
+    }
+}
