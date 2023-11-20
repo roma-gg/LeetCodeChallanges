14651a3734372cec9068cead04510a480ebb688f
Roman <rbaidachenko@gmail.com>
2023-11-20 23:17:25 +0000

    4/75

--- src/LeetCode/LeetCode_Arrays.java
+++ src/LeetCode/LeetCode_Arrays.java
@@ -36,4 +36,22 @@ public class LeetCode_Arrays {
         return result;
 
     }
+
+    // 605. Can Place Flowers
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int counter = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i-1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length-1) || (flowerbed[i+1] == 0);
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    counter++;
+                }
+            }
+        }
+
+        return n <= counter;
+    }
 }
