cd27201732c8a2dba5c5863402f3afacc2ffb8d2
Roman <rbaidachenko@gmail.com>
2023-11-22 17:07:36 +0000

    14/75

--- src/LeetCode/LeetCode_SlidingWindow.java
+++ src/LeetCode/LeetCode_SlidingWindow.java
@@ -21,4 +21,47 @@ public class LeetCode_SlidingWindow {
 
         return profit;
     }
+
+    // 643. Maximum Average Subarray I
+    public double findMaxAverage(int[] nums, int k) {
+        double subSum = 0;
+        for (int i = 0; i < k; i++) {
+            subSum += nums[i];
+        }
+
+        double maxAvg = subSum/k;
+        for (int i = 1, j = k; j < nums.length; i++, j++) {
+            subSum = subSum - nums[i-1] + nums[j];
+            maxAvg = Math.max(maxAvg, subSum/k);
+        }
+
+        return maxAvg;
+    }
+
+    // 1456. Maximum Number of Vowels in a Substring of Given Length
+    public int maxVowels(String s, int k) {
+        int maxSubVowels = 0;
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i)))
+                maxSubVowels++;
+        }
+
+        int currentVowels = maxSubVowels;
+        for (int i = 1, j = k; j < s.length(); i++, j++) {
+            if (isVowel(s.charAt(i-1)))
+                currentVowels--;
+            if (isVowel(s.charAt(j)))
+                currentVowels++;
+            maxSubVowels = Math.max(maxSubVowels, currentVowels);
+        }
+        return maxSubVowels;
+    }
+
+    private boolean isVowel(char ch2) {
+        return ch2 == 'a' ||
+                ch2 == 'e' ||
+                ch2 == 'i' ||
+                ch2 == 'o' ||
+                ch2 == 'u';
+    }
 }
