d5317e9ce1d3d67b8487fc90993651b28657770e
Roman <rbaidachenko@gmail.com>
2023-12-03 21:39:12 +0000

    21/75

--- src/LeetCode/LeetCode_Set_Map.java
+++ src/LeetCode/LeetCode_Set_Map.java
@@ -50,4 +50,43 @@ public class LeetCode_Set_Map {
 
         return map.values().size() == setOfFreq.size();
     }
+
+    // 1657. Determine if Two Strings Are Close
+    // freq of each char as value;
+    // "abbzzca"   "babzzcz"
+    //  ^           ^
+    // a2b2z2c1    b2a1z3c1
+    // "cabbba", "abbccc"
+    //  c1a2b3    a1b2c3
+    public boolean closeStrings(String word1, String word2) {
+
+        var map1 = new HashMap<Character, Integer>();
+        for (int i = 0; i < word1.length(); i++) {
+            map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i),0) + 1);
+        }
+        var map2 = new HashMap<Character, Integer>();
+        for (int i = 0; i < word2.length(); i++) {
+            map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i),0) + 1);
+        }
+
+        if (map2.size() != map1.size() || !map1.keySet().equals(map2.keySet()))
+            return false;
+
+        // can be used instead of queues
+        /*var queue1 = new PriorityQueue<Integer>(map1.values());
+        var queue2 = new PriorityQueue<Integer>(map2.values());
+
+        while (!queue1.isEmpty()) {
+            if (!queue1.remove().equals(queue2.remove()))
+                return false;
+        }*/
+
+        var word1Freq = new ArrayList<Integer>(map1.values());
+        var word2Freq = new ArrayList<Integer>(map2.values());
+        Collections.sort(word1Freq);
+        Collections.sort(word2Freq);
+
+        return word1Freq.equals(word2Freq);
+    }
+
 }
